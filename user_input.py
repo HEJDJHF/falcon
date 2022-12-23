2c9593394e2fe7088eb5ccf3669550b105ab4303
GaryChew88 <115797598+GaryChew88@users.noreply.github.com>
2022-12-23 09:36:34 +0000

    Updated user_input.py and clip_raster_file.py to specify use of OOP

--- user_input.py
+++ user_input.py
@@ -6,20 +6,20 @@ import geopandas as gpd
 
 def user_input(easting, northing):
     user_location = Point(easting, northing)
-    #  The mbr has been set to the limits of the raster file as there is no error when clipping the raster file using
-    #  rasterio's mask function.
+
+    #  OOP has been used to define the MBR and isle of wight polygons. The MBR has been set to the limits of the raster
+    #  file as there is no error when clipping the raster file using rasterio's mask function (i.e. no need to cater
+    #  for a 5km buffer).
     mbr = Polygon([(425000, 75000), (425000, 100000), (470000, 100000), (470000, 75000)])
     shapefile = gpd.read_file(os.path.join('Material', 'shape', 'isle_of_wight.shx'))
 
-    #  The mbr here is unnecessary since there is no need to cater for a 5km buffer. More important is to ensure
-    #  that the point lies on land.
     if mbr.contains(user_location) and shapefile.contains(user_location)[0]:
         is_on_land = True
     elif mbr.contains(user_location) == True and shapefile.contains(user_location)[0] == False:
-        print("The point does not lie on land.")
+        print("Please check your coordinates. The location is out at sea.")
         is_on_land = False
     else:
-        print("The location does not fall within the region.")
+        print("Please check your coordinates. The location does not fall within the region.")
         is_on_land = False
 
     return user_location, is_on_land
